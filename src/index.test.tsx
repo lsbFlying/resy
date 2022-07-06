7d1589aee341eb8375a27c53ec07bd2dc6ae4300
10019630 <liushanbao@shein.com>
2022-07-06 17:33:07 +0800

    feat: update code(更新测试文件)

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { it, expect } from "vitest";
 import { render, fireEvent, screen } from "@testing-library/react";
-import { resy } from "./index";
+import { resy, resySyncState } from "./index";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -10,25 +10,28 @@ globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 it("resy", () => {
   const store = resy({
     count: 0,
-    inc: () => store.count++,
+    inc: () => {
+      const { count } = resySyncState(store);
+      store.count = count + 1;
+    },
   });
-
+  
   const App = () => {
     const { count, inc } = store;
     return (
       <>
         <p>{count}</p>
         <button onClick={inc}>add1</button>
-        <button onClick={() => store.count++}>add2</button>
-        <button onClick={() => (store.count = +count)}>add3</button>
+        <button onClick={() => store.count = count + 2}>add2</button>
+        <button onClick={() => store.count = count + 3}>add3</button>
       </>
     );
   };
-
+  
   render(<App />);
   const error = (fn: () => void) => expect(fn).toThrow();
   const click = (btn: string) => fireEvent.click(screen.getByText(btn));
-
+  
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   error(() => resy());

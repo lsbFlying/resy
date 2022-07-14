9ca67ee2e841c4276668a7428d7b8c0b480c261b
10019630 <liushanbao@shein.com>
2022-07-14 20:52:08 +0800

    feat: update code(更新支持react-native)

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,23 @@
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import pkg from "./package.json";
+import replacePlugin from "rollup-plugin-replace";
+import resolvePlugin from "@rollup/plugin-node-resolve";
+// @ts-ignore
+import babelPlugin from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser"; // 压缩打包文件
 
 const input = "src/index.ts";
-const deps = Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies });
-const external = (id: string) => deps.some((dep) => id.startsWith(dep));
-const plugins = [typescript(), terser()];
+// const deps = Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies });
+// const external = (id: string) => deps.some((dep) => id.startsWith(dep));
+const external = ["use-sync-external-store/shim", "react", "./react-platform"];
+const plugins = [
+  replacePlugin({ "react-platform": "./react-platform" }),
+  resolvePlugin(),
+  babelPlugin({ exclude: 'node_modules/**' }),
+  typescript(),
+  terser(),
+];
 
 // 打包文件的头部声明
 const banner =
@@ -18,11 +29,47 @@ const banner =
   ` * Released under the ${pkg.license} License.\n` +
   " */";
 
-const cjsOutput = { file: pkg.main, format: "cjs", banner, exports: "auto" };
-const esmOutput = { file: pkg.module, format: "es", banner };
+const cjsOutput = { file: pkg.main, format: "cjs", exports: "auto" };
+const esmOutput = { file: pkg.module, format: "es" };
 const dtsOutput = { file: pkg.types, format: "es", banner };
 
+const platformBuild = [
+  {
+    input: "src/platforms/dom.ts",
+    external: ["react-dom"],
+    output: [
+      {
+        format: "es",
+        dir: "dist",
+        entryFileNames: "react-platform.js",
+      },
+      {
+        format: "cjs",
+        dir: "dist",
+        entryFileNames: "react-platform.cjs.js",
+      },
+    ],
+  },
+  {
+    input: "src/platforms/native.ts",
+    external: ["react-native"],
+    output: [
+      {
+        format: "es",
+        dir: "dist",
+        entryFileNames: "react-platform.native.js",
+      },
+      {
+        format: "cjs",
+        dir: "dist",
+        entryFileNames: "react-platform.cjs.native.js",
+      },
+    ],
+  },
+];
+
 export default [
+  ...platformBuild,
   { input, output: cjsOutput, external, plugins },
   { input, output: esmOutput, external, plugins },
   { input, output: dtsOutput, plugins: [dts()] },
--- src/static.ts
+++ src/static.ts
@@ -1,4 +1,5 @@
-import ReactDOM from "react-dom";
+// @ts-ignore
+import { unstable_batchedUpdates } from "react-platform";
 import { Callback } from "./model";
 
 /**
@@ -11,7 +12,7 @@ export function batchUpdateShimRun(fn: Callback) { fn() }
  * React-v18中所有的更新以及是自动化批处理的了，但是unstable_batchedUpdates这个API它目前还仍然在18的版本中可以使用，
  * 但不保证未来会有去除这个非正式API的可能性，所以做一个垫片保证代码的安全稳健性
  */
-export const batchUpdate = ReactDOM.unstable_batchedUpdates || batchUpdateShimRun;
+export const batchUpdate = unstable_batchedUpdates || batchUpdateShimRun;
 
 // 某一个store的监听订阅对象、内部state数据的唯一标识key值
 export const storeListenerStateKey = Symbol("resyStoreListenerSymbolKey");

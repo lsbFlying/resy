65276204ec4b93d094b338ef62754826928d5c25
10019630 <liushanbao@shein.com>
2022-10-13 15:23:35 +0800

    feat: refine code and upgrade notes

--- src/listener.ts
+++ src/listener.ts
@@ -17,10 +17,11 @@ export interface CustomEventListener<T extends State> {
     nextState: T,
   ): void,
   /**
-   * 本身EventDispatcher可以单独使用，在结合resy时销毁监听订阅的时候实际上是移除了监听Set中的监听实例
+   * 本身EventDispatcher可以单独使用，在结合resy是销毁监听订阅的时候实际上是移除了监听Set中的监听实例
    * 所以subscribe这里可以不用多余使用removeEventListener，直接移除实例即可
+   * 所以这里也是直接简化去除removeEventListener
    */
-  removeEventListener(type: string | symbol): void,
+  // removeEventListener(type: string | symbol): void,
 }
 
 /**
@@ -34,9 +35,13 @@ export function EventDispatcher(this: any) {
 EventDispatcher.prototype.addEventListener = function<T extends State>(this: any, type: string, handle: Listener<T>) {
   this.events[type] = handle;
 }
-EventDispatcher.prototype.removeEventListener = function(type: string | symbol) {
-  this.events[type] = undefined;
-}
+/**
+ * 刚好也正是由于resy是销毁监听订阅的时候实际上是移除了监听Set中的监听实例
+ * 所以这里也可以简化去除removeEventListener
+ */
+// EventDispatcher.prototype.removeEventListener = function(type: string | symbol) {
+//   this.events[type] = undefined;
+// }
 EventDispatcher.prototype.dispatchEvent = function<T extends State>(
   this: any,
   type: string,
@@ -44,6 +49,13 @@ EventDispatcher.prototype.dispatchEvent = function<T extends State>(
   prevState: T,
   nextState: T,
 ) {
-  if (!this.events[type]) return;
+  /**
+   * 由于resy是销毁监听订阅的时候实际上是移除了监听Set中的监听实例
+   * 所以这里原本作为单独使用的代码可以把if语句去掉直接执行
+   * remove next code
+   * ```code
+   * if (!this.events[type]) return;
+   * ```
+   */
   this.events[type](effectState, prevState, nextState);
 }

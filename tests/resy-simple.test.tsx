1a1925c54ae4f2b025a16ae9150a415b45c76eb0
10019630 <liushanbao@shein.com>
2022-10-19 20:05:54 +0800

    feat: update test file

--- tests/resy-simple.test.tsx
+++ tests/resy-simple.test.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { expect, test } from "vitest";
 import { createStore, useStore } from "../src";
 import { fireEvent, render } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 
 test("resy-simple", async () => {
-  const store = createStore({ count: 0 });
+  const store = createStore<{
+    count: number;
+    count2: number;
+    formRef?: any;
+  }>({ count: 0, count2: 123, });
+  
+  const AppTest = () => {
+    const { count2, formRef } = useStore(store, { formRef: React.useRef<any>() });
+    
+    function formBtnClick() {
+      formRef.current.value = "QWE";
+    }
+    
+    useEffect(() => {
+      store.count2 = 999;
+    }, [formRef.current]);
+    
+    return (
+      <form>
+        name: <input id="testInput" ref={formRef} type="text" name="Name"/><br/>
+        <button onClick={formBtnClick}>form-button</button>
+        <span>{count2}</span>
+      </form>
+    );
+  }
+  
   const App = () => {
     const state = useStore(store);
     return (
       <>
         <p>{state.count}</p>
         <button onClick={() => store.count++}>inc-btn</button>
+        <AppTest/>
       </>
     );
   };
@@ -29,4 +55,9 @@ test("resy-simple", async () => {
    * 所以这里需要等待后才能获取到页面的最新渲染
    */
   expect(getByText("1")).toBeInTheDocument();
+  
+  await act(() => {
+    fireEvent.click(getByText("form-button"));
+  });
+  expect(getByText("999")).toBeInTheDocument();
 });

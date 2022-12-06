328e07c5fbe5bdc4775db52b9450214a0fd45e30
10019630 <liushanbao@shein.com>
2022-12-06 18:26:16 +0800

    upgrade test file

--- tests/resy-privatization.test.tsx
+++ tests/resy-privatization.test.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { test } from "vitest";
-import { createStore, useStore } from "../src";
+import { useStore, usePrivateStore } from "../src";
 import { fireEvent, render, waitFor } from "@testing-library/react";
 
 test("resy-simple", async () => {
@@ -16,7 +16,7 @@ test("resy-simple", async () => {
      * 乍一看下面两句代码反而没有react原生方式有优势
      * 但实际上在超过两个以上的数据状态的情况下，下面的写法的优势是直线上升
      */
-    const store = useMemo(() => createStore(initialState, { privatization: true }), []);
+    const store = usePrivateStore(initialState);
     const { count, text } = useStore(store);
     return (
       <>

81b923d8a98fc8c95beeab679b4165fc390c6ade
10019630 <liushanbao@shein.com>
2023-03-13 15:16:30 +0800

    update notes

--- src/use.ts
+++ src/use.ts
@@ -26,7 +26,7 @@ export function useStore<S extends State>(store: S): S {
  * const [text, setText] = useState("hello");
  * 🌟: useConciseState相对于useState在多个数据状态时使用相对简单明了
  * 🌟:同时 useConciseState中可以解析出store属性，通过store可以读取各个数据的最新数据值
- * 弥补了useState中无法读取属性数据的最新值的不足
+ * 弥补了useState中无法读取属性数据的最新值的不足，这是最核心的关键点
  */
 export function useConciseState<S extends State>(initialState?: S): ConciseStore<S> {
   const ref = useRef(initialState);

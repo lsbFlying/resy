335ac98b63ff1b24271f2b4947dee394951d38b7
10019630 <liushanbao@shein.com>
2023-03-15 11:47:33 +0800

    feat: upgrade notes

--- src/createStore.ts
+++ src/createStore.ts
@@ -284,7 +284,7 @@ export function createStore<S extends State>(
        */
       scheduler.set("updateIsOn", Promise.resolve().then(() => {
         scheduler.set("updateIsOn", undefined);
-  
+        
         const { taskDataMap, taskQueueMap } = (scheduler.get("getTask") as Scheduler<S>["getTask"])(
           taskDataMapPrivate,
           taskQueueMapPrivate,
@@ -292,13 +292,25 @@ export function createStore<S extends State>(
         // 至此，这一轮数据更新的任务完成，立即清空冲刷任务数据与任务队列，腾出空间为下一轮数据更新做准备
         (scheduler.get("flush") as Scheduler<S>["flush"])(taskDataMapPrivate, taskQueueMapPrivate);
         if (taskDataMap.size !== 0) {
-          // 更新之前的数据
+          // 未更新之前的数据
           const prevState = new Map(stateMap);
           batchUpdate(() => taskQueueMap.forEach(task => task()));
           if ((storeCoreMap.get("listenerStoreSet") as StoreCoreMapValue<S>["listenerStoreSet"]).size) {
             batchDispatchListener(prevState, taskDataMap);
           }
         }
+        
+        /**
+         * 先更新，再执行回调
+         * 这样会导致如果是多个setState同步代码执行，
+         * 则后续回调执行的时候第一个回调内通过store读取数据也可以获取后续更新的数据最新值
+         * 而如果是setState的回调中嵌套执行了setState，内部嵌套的setState再次有可执行回调
+         * 则第一轮回调无法获取第二轮更新的数据最新值，这也是符合逻辑的
+         * 与此同时，这两点与class组件的this.setState的回调效果是一致的，
+         * 而setState的回调callback的参数nextState的存在
+         * 就是为了解决事实上多个setState同步代码执行存在store读取数据产生最新数据值的问题
+         * 可见react中的class组件的this.setState的回调函数也是通过大致执行入栈出栈的思路完成的
+         */
         setStateCallbackStackArray.forEach((
           {callback, cycleData: { updateParams, cycleState }}, index, array,
         ) => {

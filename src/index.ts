98911784d3a391bec23a950a6c623c1e83e3b8c0
liushanbao <1262300490@qq.com>
2023-07-16 17:13:25 +0800

    feat: upgrade code

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./createStore";
 export * from "./use";
 export * from "./view";
 export type {
-  CreateStoreOptions, MapStateToProps, Store, ConciseStore, PS, State,
+  CreateStoreOptions, MapStateToProps, Store, ConciseStore, PS, State, StoreUtils,
+  SetState, SyncUpdate, Restore, Subscribe,
 } from "./model";

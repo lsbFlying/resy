9e598332e3cadea17264d4a24ecd77720848445e
liushanbao <1262300490@qq.com>
2023-12-14 20:31:15 +0800

    upgrade test file

--- tests/update/index4.test.tsx
+++ tests/update/index4.test.tsx
@@ -19,13 +19,65 @@ test("batchUpdate-IV", async () => {
         <button onClick={() => {
           store.count++;
           expect(store.count === 1).toBeTruthy();
-          // The update of setTimeout and the previous sentence is divided into two batches of update processing,
-          // which shows that the update mechanism of store is based on the event cycle.
-          const id = setTimeout(() => {
-            clearTimeout(id);
+          // The update mechanism of store is based on the event cycle.
+          Promise.resolve().then(() => {
+            // batch handle, merge into one update
+            store.count++;
+            expect(store.count === 2).toBeTruthy();
+            store.count++;
+            expect(store.count === 3).toBeTruthy();
+          });
+          Promise.resolve().then(() => {
             store.setState({
               count: store.count + 1,
             });
+            expect(store.count === 4).toBeTruthy();
+
+            store.setState({
+              count: store.count + 1,
+            });
+            expect(store.count === 5).toBeTruthy();
+          });
+          Promise.resolve().then(() => {
+            store.count++;
+            expect(store.count === 6).toBeTruthy();
+
+            store.setState({
+              count: store.count + 1,
+            });
+            expect(store.count === 7).toBeTruthy();
+          });
+
+          const id = setTimeout(() => {
+            clearTimeout(id);
+            store.count++;
+            expect(store.count === 8).toBeTruthy();
+
+            store.count++;
+            expect(store.count === 9).toBeTruthy();
+          }, 0);
+          const id2 = setTimeout(() => {
+            clearTimeout(id2);
+            store.setState({
+              count: store.count + 1,
+            });
+            expect(store.count === 10).toBeTruthy();
+
+            store.setState({
+              count: store.count + 1,
+            });
+            expect(store.count === 11).toBeTruthy();
+          }, 0);
+          const id3 = setTimeout(() => {
+            clearTimeout(id3);
+            // Mixed updates, merge into one update
+            store.count++;
+            expect(store.count === 12).toBeTruthy();
+
+            store.setState({
+              count: store.count + 1,
+            });
+            expect(store.count === 13).toBeTruthy();
           }, 0);
         }}>batchAdd</button>
       </>
@@ -36,7 +88,10 @@ test("batchUpdate-IV", async () => {
 
   fireEvent.click(getByText("batchAdd"));
   await waitFor(() => {
-    getByText("2");
-    expect(counter === 3).toBeTruthy();
+    getByText("13");
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      expect(counter === 8).toBeTruthy();
+    }, 0);
   });
 });

d97f8e768b1b31d483f1dc6a20a3854b5aa660f4
liushanbao <1262300490@qq.com>
2024-02-25 16:12:26 +0800

    publish ner version 11.0.0-alpha.2

--- src/static.ts
+++ src/static.ts
@@ -1,12 +1,14 @@
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
-import { unstable_batchedUpdates } from "react-platform";
+import ReactPlatformExports from "react-platform";
 import type { Callback } from "./types";
 
 const NODE_ENV = process.env.NODE_ENV;
 if (!NODE_ENV) console.error("NODE_ENV not set");
 
 export const __DEV__ = NODE_ENV === "development";
+
+const { unstable_batchedUpdates } = ReactPlatformExports;
 
 /** Batch processing safety shim */
 export const batchUpdateShimRun = (fn: Callback) => fn();

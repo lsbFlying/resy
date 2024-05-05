030fdcd4a4c9e2bcecc49a6395191299490b4cea
liushanbao <1262300490@qq.com>
2024-05-05 14:26:19 +0800

    upgrade type

--- src/store/types.ts
+++ src/store/types.ts
@@ -195,10 +195,10 @@ export interface UseStore<S extends PrimitiveState> {
  * Additionally, an urgent issue is that `value` cannot address the access of
  * function properties to the `value` value.
  */
-export type SignalMeta<T extends ReactNode> = T & ReactNode & { valueOf(): T };
+export type Signal<T extends ReactNode> = T & ReactNode & { valueOf(): T };
 
 export type SignalsType<S extends PrimitiveState> = {
-  [K in keyof S]: SignalMeta<S[K]>;
+  [K in keyof S]: Signal<S[K]>;
 };
 
 /** The type of signal-store returned by useSignal */

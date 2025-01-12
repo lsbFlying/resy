e9a9ea77701f096da58bf6fdca194ca4134327a0
liushanbao <1262300490@qq.com>
2025-01-12 16:08:36 +0800

    sync update next-release-mutate branch code

--- develop/App.tsx
+++ develop/App.tsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { defineStore } from "../src";
 
 interface Model {
   count: number;
+  info: {
+    nameInfo: {
+      name: string;
+    };
+  };
   increase(): void;
+  updateInfo(): void;
 }
 
 const useStore = defineStore<Model>({
   count: 0,
+  info: {
+    nameInfo: {
+      name: "Jack",
+    },
+  },
   increase() {
     this.count++;
   },
-});
+  updateInfo() {
+    this.info.nameInfo.name = "Bob";
+    // const { info } = this;
+    // const { nameInfo } = info;
+    // const { name } = nameInfo;
+    // console.log("this(store)", info, nameInfo, name);
+    // const value = this.info;
+    // this.setState({
+    //   info: {
+    //     ...value,
+    //     nameInfo: {
+    //       name: "Bob"
+    //     },
+    //   },
+    // });
+  },
+}, { immutable: true });
 
 const App = () => {
-  const { count, increase } = useStore();
+  const { count, info, increase, updateInfo } = useStore();
+  const { nameInfo } = info;
+  const { name } = nameInfo;
+  console.log("render(engine)", info, nameInfo, name);
+
+  useEffect(() => {
+    console.log(123123);
+  }, [nameInfo]);
 
   return (
     <>
       <p>count:{count}</p>
       <button onClick={increase}>increase</button>
+      <p>info-name:{name}</p>
+      <button onClick={updateInfo}>updateInfo</button>
     </>
   );
 };

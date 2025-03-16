b0e65871fb4eaf975f23855288f451edc7e23cd0
liushanbao <1262300490@qq.com>
2025-03-16 17:06:14 +0800

    remove error eslint-disable-next-line

--- src/store/index.ts
+++ src/store/index.ts
@@ -331,7 +331,6 @@ export const createStore = <S extends PrimitiveState>(
         }
 
         if (typeof value === "function" && !(value as AnyBoundFn).__bound__) {
-          // eslint-disable-next-line @typescript-eslint/no-use-before-define
           return boundFnProcessing(key, value, target, stateMap, store);
         }
 

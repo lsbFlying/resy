811afacac1dedf18d7069dbe2e811c532a92a823
liushanbao <1262300490@qq.com>
2025-05-08 16:01:28 +0800

    update index.js notes for eslint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 if (process.env.NODE_ENV === "production") {
-  // eslint-disable-next-line import/extensions
+  // eslint-disable-next-line import/extensions,@typescript-eslint/no-require-imports
   module.exports = require("./dist/resy.cjs.prod.js");
 } else {
-  // eslint-disable-next-line import/extensions
+  // eslint-disable-next-line import/extensions,@typescript-eslint/no-require-imports
   module.exports = require("./dist/resy.cjs.js");
 }

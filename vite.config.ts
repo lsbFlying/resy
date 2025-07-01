ca6a0e844da7136b2541cd24191217cc063d56be
liushanbao <1262300490@qq.com>
2025-07-01 17:32:29 +0800

    update notes

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
     host: "localhost",
   },
   define: {
+    /**
+     * @desc At present, the project dependency package RN contains
+     * a globally declared __DEV__ variable type declaration,
+     * which can be used directly without the need to
+     * create a ` vite-env. d.ts ` file for global variable type declaration.
+     */
     __DEV__: "true",
   },
   resolve: {

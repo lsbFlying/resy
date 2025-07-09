9de2b7c0eb70d1b91f1a5347f65cb289c2b91f3c
liushanbao <1262300490@qq.com>
2025-07-09 20:16:00 +0800

    add TODO

--- src/computer/utils.ts
+++ src/computer/utils.ts
@@ -2,7 +2,10 @@ import type { Store } from "../store/types";
 import type { AnyFn, PrimitiveState } from "../types";
 import { storeErrorProcessing } from "../store/errors";
 
-// computed utils
+/**
+ * @desc computed utils
+ * TODO waiting developing
+ */
 export const computed = <S extends PrimitiveState, T extends AnyFn>(
   store: Store<S>,
   fn: T,

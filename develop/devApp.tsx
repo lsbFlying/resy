56a3e107c97f99d9c0764d6a8e7e4076c8dfc978
liushanbao <1262300490@qq.com>
2025-11-03 22:00:13 +0800

    feat: refine code

--- develop/devApp.tsx
+++ develop/devApp.tsx
@@ -1,39 +1,95 @@
-import { ComponentWithStore, createStore } from "../src";
-import React from "react";
+import { createStore, useStore } from "../src";
+import React, { useEffect } from "react";
 
-console.time("createStore");
+let fineGrainedCounterNormal = 0;
+
 const store = createStore({
   count: 0,
   text: "hello world",
-  doubleCount() {
-    console.log("doubleCount");
-    return this.count * 2;
-  }
+  readyOk: false,
+  readyOk2: false,
 });
-console.timeEnd("createStore");
 
-class App extends ComponentWithStore {
+// changes in the state of count data will not cause re-render in Text
+function Text() {
+  // console.log("render-Text");
+  const { text } = useStore(store);
+  // console.log("render-Text-after");
 
-  store = this.connectStore(store);
+  useEffect(() => {
+    console.log("useEffect-Text");
+  }, []);
 
-  render() {
-    const { count, text, doubleCount } = this.store;
-    return (
-      <>
-        <p>count:{count}</p>
-        <p>doubleCount:{doubleCount()}</p>
-        <p>text:{text}</p>
-        <button onClick={() => {
-          store.count++;
-        }}>add
-        </button>
-        <button onClick={() => {
-          store.text = "hello";
-        }}>text
-        </button>
-      </>
-    );
-  }
+  fineGrainedCounterNormal++;
+  return <p>{text}</p>;
 }
+
+// changes in the state of text data will not cause re-render in Count
+function Count() {
+  // console.log("render-Count");
+  const { count } = useStore(store);
+  // console.log("render-Count-after");
+
+  useEffect(() => {
+    console.log("useEffect-Count");
+  }, []);
+
+  fineGrainedCounterNormal++;
+  return <p>{count}</p>;
+}
+
+const App = () => {
+  // console.log("render-App");
+  const { readyOk, readyOk2 } = useStore(store);
+  // 隐式调用useHandle，useStore写完执行完之后默认隐式调用useHandle，但是不用写useHandle
+  // useHandle();
+  // console.log("render-App-after");
+
+  useEffect(() => {
+    console.log("useEffect-App");
+  }, []);
+
+  fineGrainedCounterNormal++;
+  return (
+    <>
+      <Count />
+      <Text />
+      <p>{readyOk ? "ok" : "loading"}</p>
+      <button onClick={() => {
+        store.count++;
+      }}>countChange</button>
+      <button onClick={() => {
+        store.text = "good bye";
+      }}>textChange</button>
+      <button onClick={() => {
+        store.readyOk = true;
+      }}>alreadyChange</button>
+    </>
+  );
+};
+
+// class App extends ComponentWithStore {
+//
+//   store = this.connectStore(store);
+//
+//   render() {
+//     const { count, text, doubleCount } = this.store;
+//     return (
+//       <>
+//         <p>count:{count}</p>
+//         <p>doubleCount:{doubleCount()}</p>
+//         <p>text:{text}</p>
+//         <button onClick={() => {
+//           store.count++;
+//         }}>add
+//         </button>
+//         <button onClick={() => {
+//           store.text = "hello";
+//         }}>text
+//         </button>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
